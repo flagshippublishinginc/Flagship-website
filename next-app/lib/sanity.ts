9275802bf34238691c8012791819c178ad5bdb70
krishan-gtmn <krishan@growtomation.in>
2025-12-17 14:02:31 +0530

    updated sanity

--- next-app/lib/sanity.ts
+++ next-app/lib/sanity.ts
@@ -8,7 +8,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2025-12-10",
-  useCdn: !isPreview, // Disable CDN in preview to see latest changes
+  useCdn: !isPreview,
   perspective: isPreview ? "previewDrafts" : "published",
   token: isPreview ? process.env.NEXT_PUBLIC_SANITY_TOKEN : undefined,
   ignoreBrowserTokenWarning: true,
